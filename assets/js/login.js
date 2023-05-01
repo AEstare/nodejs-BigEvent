7dea63e31b948a85b59f3a746f167a9fa7d9b66c
Ae <2662534305@qq.com>
2023-05-01 15:44:44 +0800

    5.1、完成了登陆和注册功能的开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -13,6 +13,7 @@ $(function(){
 
     // 从layui获取form对象
     const form=layui.form
+    const layer=layui.layer
     // 通过form.verify()来自定义校验规则
     form.verify({
         // 自定义一个叫做pwd的校验规则
@@ -29,4 +30,44 @@ $(function(){
             }
         }
     })
+
+
+    // 监听注册表单的提交事件
+    $('#form_reg').on('submit',function(e){
+        // 阻止默认提交
+        e.preventDefault()
+        // 发起ajax请求
+        const data={username:$('#form_reg [name=username]').val(),password:$('#form_reg [name=password]').val()}
+        $.post('/api/reguser',data,function(res){
+            if(res.status!==0) {
+                // return console.log(res.message);
+                return layer.msg(res.message)
+            }
+            layer.msg('注册成功，请登陆')
+            // 模拟点击，跳转到登陆页面
+            $('#link_login').click()
+        })
+    })
+
+    // 监听登陆表单提交事件
+    $('#form_login').submit(function(e){
+        // 阻止默认提交行为
+        e.preventDefault()
+        // 发起ajax请求
+        $.ajax({
+            url:'/api/login',
+            method:'POST',
+            // 快速获取表单数据
+            data:$(this).serialize(),
+            success:function(res){
+                if(res.status!==0) return layer.msg('登陆失败')
+                layer.msg('登陆成功')
+                // 将登陆成功后的token保存到localStorage中
+                localStorage.setItem('token',res.token)
+                // 跳转到后台主页
+                location.href='../../index.html'
+            }
+        })
+    })
 })
+
